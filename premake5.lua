e71bc2bd586a7266d25d167eb5c34b898c639a09
rbaldi <rbaldi@34bigthings.com>
2022-03-04 17:48:27 +0100

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -15,7 +15,12 @@ kind "StaticLib"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
         "src/osmesa_context.c",
+        "src/platform.c",
         "src/vulkan.c",
         "src/window.c"
     }
@@ -27,6 +32,7 @@ kind "StaticLib"
         files {
             "src/win32_init.c",
             "src/win32_joystick.c",
+            "src/win32_module.c",
             "src/win32_monitor.c",
             "src/win32_thread.c",
             "src/win32_time.c",
@@ -48,11 +54,6 @@ kind "StaticLib"
         files {
             "src/glx_context.c",
             "src/linux_joystick.c",
-            "src/null_init.c",
-            "src/null_joystick.c",
-            "src/null_monitor.c",
-            "src/null_window.c",
-            "src/platform.c",
             "src/posix_module.c",
             "src/posix_time.c",
             "src/posix_thread.c",
